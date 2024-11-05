7081071d0ab6dcf71bd2e8ed9eb27431a21e830e
Claudia de Luna <cldeluna@yahoo.com>
2024-11-05 12:17:16 -0800

    Examples of modular

--- modular_include_config_generator.py
+++ modular_include_config_generator.py
@@ -18,45 +18,37 @@ import jinja2
 
 def load_cfg_data():
     config_data = {
-        'hostname': 'ROUTER-1',
-        'tacacs_server': 'TACACS-1',
-        'tacacs_ip': '192.168.1.100',
-        'tacacs_key': 'tacacs123',
-        'local_users': [
-            {'name': 'admin', 'privilege': 15, 'password': 'admin123'},
-            {'name': 'operator', 'privilege': 5, 'password': 'oper123'}
+        "hostname": "ROUTER-1",
+        "tacacs_server": "TACACS-1",
+        "tacacs_ip": "192.168.1.100",
+        "tacacs_key": "tacacs123",
+        "local_users": [
+            {"name": "admin", "privilege": 15, "password": "admin123"},
+            {"name": "operator", "privilege": 5, "password": "oper123"},
         ],
-        'interfaces': [
+        "interfaces": [
             {
-                'name': 'GigabitEthernet0/0',
-                'description': 'WAN Interface',
-                'ip_address': '10.1.1.1',
-                'subnet_mask': '255.255.255.0',
-                'ospf_area': '0'
+                "name": "GigabitEthernet0/0",
+                "description": "WAN Interface",
+                "ip_address": "10.1.1.1",
+                "subnet_mask": "255.255.255.0",
+                "ospf_area": "0",
             },
             {
-                'name': 'GigabitEthernet0/1',
-                'description': 'LAN Interface',
-                'ip_address': '192.168.1.1',
-                'subnet_mask': '255.255.255.0',
-                'ospf_area': '1'
-            }
-        ],
-        'ospf_process_id': 1,
-        'router_id': '1.1.1.1',
-        'ospf_networks': [
-            {
-                'prefix': '10.1.1.0',
-                'wildcard': '0.0.0.255',
-                'area': '0'
+                "name": "GigabitEthernet0/1",
+                "description": "LAN Interface",
+                "ip_address": "192.168.1.1",
+                "subnet_mask": "255.255.255.0",
+                "ospf_area": "1",
             },
-            {
-                'prefix': '192.168.1.0',
-                'wildcard': '0.0.0.255',
-                'area': '1'
-            }
         ],
-        'redistribute': ['connected', 'static']
+        "ospf_process_id": 1,
+        "router_id": "1.1.1.1",
+        "ospf_networks": [
+            {"prefix": "10.1.1.0", "wildcard": "0.0.0.255", "area": "0"},
+            {"prefix": "192.168.1.0", "wildcard": "0.0.0.255", "area": "1"},
+        ],
+        "redistribute": ["connected", "static"],
     }
 
     return config_data
@@ -69,25 +61,25 @@ def main():
 
     # Create Jinja2 environment
     env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader('templates'),
+        loader=jinja2.FileSystemLoader("templates"),
         trim_blocks=True,
-        lstrip_blocks=True
+        lstrip_blocks=True,
     )
 
     # Load base template
-    template = env.get_template('modular_inc_base.j2')
+    template = env.get_template("modular_inc_base.j2")
 
     # Render configuration
     config = template.render(cfg_data)
 
     # Write configuration to file
     file_name = f"{cfg_data['hostname']}_router_config.txt"
-    with open(file_name, 'w') as f:
+    with open(file_name, "w") as f:
         f.write(config)
 
     print(f"\nSaved configuration to file {file_name} in current directory.\n")
 
 
 # Standard call to the main() function.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- modular_inheritance_config_generator.py
+++ modular_inheritance_config_generator.py
@@ -18,39 +18,31 @@ import jinja2
 
 def load_cfg_data():
     config_data = {
-        'hostname': 'ROUTER-2',
-        'tacacs_server': '192.168.1.10',
-        'tacacs_key': 'secret123',
-        'interfaces': [
+        "hostname": "ROUTER-2",
+        "tacacs_server": "192.168.1.10",
+        "tacacs_key": "secret123",
+        "interfaces": [
             {
-                'name': 'GigabitEthernet0/1',
-                'description': 'Link to Core',
-                'ip_address': '10.1.1.1',
-                'subnet_mask': '255.255.255.0',
-                'ospf_area': '0'
+                "name": "GigabitEthernet0/1",
+                "description": "Link to Core",
+                "ip_address": "10.1.1.1",
+                "subnet_mask": "255.255.255.0",
+                "ospf_area": "0",
             },
             {
-                'name': 'GigabitEthernet0/2',
-                'description': 'Link to Distribution',
-                'ip_address': '10.1.2.1',
-                'subnet_mask': '255.255.255.0',
-                'ospf_area': '1'
-            }
+                "name": "GigabitEthernet0/2",
+                "description": "Link to Distribution",
+                "ip_address": "10.1.2.1",
+                "subnet_mask": "255.255.255.0",
+                "ospf_area": "1",
+            },
         ],
-        'ospf_process_id': 100,
-        'router_id': '1.1.1.1',
-        'ospf_networks': [
-            {
-                'prefix': '10.1.1.0',
-                'wildcard': '0.0.0.255',
-                'area': '0'
-            },
-            {
-                'prefix': '10.1.2.0',
-                'wildcard': '0.0.0.255',
-                'area': '100'
-            }
-        ]
+        "ospf_process_id": 100,
+        "router_id": "1.1.1.1",
+        "ospf_networks": [
+            {"prefix": "10.1.1.0", "wildcard": "0.0.0.255", "area": "0"},
+            {"prefix": "10.1.2.0", "wildcard": "0.0.0.255", "area": "100"},
+        ],
     }
 
     return config_data
@@ -63,41 +55,41 @@ def main():
 
     # Create Jinja2 environment
     env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader('templates'),
+        loader=jinja2.FileSystemLoader("templates"),
         trim_blocks=True,
-        lstrip_blocks=True
+        lstrip_blocks=True,
     )
 
     # Load templates
-    base_template = env.get_template('mod_inherit_base.j2')
-    aaa_template = env.get_template('mod_inherit_aaa.j2')
-    interfaces_template = env.get_template('mod_inherit_interfaces.j2')
-    ospf_template = env.get_template('mod_inherit_ospf.j2')
+    base_template = env.get_template("mod_inherit_base.j2")
+    aaa_template = env.get_template("mod_inherit_aaa.j2")
+    interfaces_template = env.get_template("mod_inherit_interfaces.j2")
+    ospf_template = env.get_template("mod_inherit_ospf.j2")
 
     # Generate configurations
     config_sections = {
-        'base': base_template.render(cfg_data),
-        'aaa': aaa_template.render(cfg_data),
-        'interfaces': interfaces_template.render(cfg_data),
-        'ospf': ospf_template.render(cfg_data)
+        "base": base_template.render(cfg_data),
+        "aaa": aaa_template.render(cfg_data),
+        "interfaces": interfaces_template.render(cfg_data),
+        "ospf": ospf_template.render(cfg_data),
     }
 
-    print(config_sections['base'])
-    print(config_sections['aaa'])
-    print(config_sections['interfaces'])
-    print(config_sections['ospf'])
+    print(config_sections["base"])
+    print(config_sections["aaa"])
+    print(config_sections["interfaces"])
+    print(config_sections["ospf"])
 
     # Write configuration to file
     file_name = f"{cfg_data['hostname']}_router_config.txt"
-    with open(file_name, 'w') as f:
-        f.write(config_sections['base'])
-        f.write(config_sections['aaa'])
-        f.write(config_sections['interfaces'])
-        f.write(config_sections['ospf'])
+    with open(file_name, "w") as f:
+        f.write(config_sections["base"])
+        f.write(config_sections["aaa"])
+        f.write(config_sections["interfaces"])
+        f.write(config_sections["ospf"])
 
     print(f"\nSaved configuration to file {file_name} in current directory.\n")
 
 
 # Standard call to the main() function.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
