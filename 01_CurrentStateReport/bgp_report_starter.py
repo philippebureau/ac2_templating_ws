9b6da74732ad22ec20872882b82890d1c0ebfa65
Claudia de Luna <cldeluna@yahoo.com>
2024-11-11 12:14:51 -0800

    Added starter script to get the project going

--- 01_CurrentStateReport/bgp_report_starter.py
+++ 01_CurrentStateReport/bgp_report_starter.py
@@ -28,7 +28,6 @@ sys.path.append(parent_dir)
 # Now you can import the module from the parent directory
 import utils
 
-
 def create_bgp_diagram(bgp_sessions, filename="bgp_sessions", outformat="png"):
     """
     Given a list of dictionaries in bgp_sessions with local and peer information, draw a digram which
@@ -68,94 +67,45 @@ def create_bgp_diagram(bgp_sessions, filename="bgp_sessions", outformat="png"):
             routers[local_key] - Edge(label=edge_label) - routers[peer_key]
 
 
-def get_template_selection(options):
-    """
-    Function to enumerate templates in the templates directory for interactive user selection
-    :param options:
-    :return: selected template
-    """
-    # Display numbered options
-    for index, item in enumerate(options, start=1):
-        print(f"{index}. {item}")
-
-    # Get user input and validate
-    while True:
-        try:
-            choice = int(input("Enter the number of your choice: "))
-            if 1 <= choice <= len(options):
-                return options[choice - 1]
-            else:
-                print("Invalid choice. Please try again.")
-        except ValueError:
-            print("Please enter a valid number.")
-
-
-
 def main():
 
     # Step 1: Create Environment
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
+    # Set template file (this file should be in the templates directory under 01_CurrentStateReport
     selected_template = "bgp_report_md.j2"
 
-    # ====  START Example of an interactive CLI to pick the template
-    # What templates are available in this environment?
-    # template_list = env.list_templates()
-
-    # Print the list of templates available in the defined environment
-    # which is looking at a relative path in the "templates" directory
-    # for template in template_list:
-    #     print(template)
-
-    # selected_template = get_template_selection(template_list)
-    # print(f"You selected: {selected_template}")
-    # ====  END Example of an interactive CLI to pick the template
-
-    # Step 2: Load the template
-    bgp_rpt_template = env.get_template(selected_template)
-
-    # This is only needed to display the template in the Terminal
-    bgp_rpt_template.source = open(bgp_rpt_template.filename).read()
-    print(f"\n\n======= EXECUTING BGP Report Script=======")
-    print(f"\nTemplate name: {bgp_rpt_template.name}")
-    print(f"Template filename: {bgp_rpt_template.filename}")
-
-    print(f"\nTemplate {selected_template} Source:")
-    print(bgp_rpt_template.source)
+    # Step 2: Load the template int memory (Tip:  Slide 19) using the variable bgp_rpt_template
 
     # Manipulate the data
     # In this case we have queried SuzieQ for a list of BGP sessions at the GDL_Campus
     # and saved the output to a JSON file for later use
 
-    # Load the JSON file
-    data = utils.load_json("GDL_bgp.json")
+    # Load the JSON file GDL_bgp.json into a variable named data
+    # Calculate a boolean all_peers_up (bool)
+    # Calculate the number of iBGP sessions in ibgp_count (int)
+    # Calculate the number of eBGP sessions in ebgp_count (int)
+    # Tip: Take a look at the JSON file with the data and determine how you are going to process it to get the
+    # values above
 
-    # Boolean to see if all sessions are up
-    all_peers_up = True
-    ibgp_count = 0
-    ebgp_count = 0
-    for line in data:
-        if "Down" in line["state"]:
-            all_peers_up = False
-        if line["asn"] == line["peerAsn"]:
-            ibgp_count += 1
-        else:
-            ebgp_count += 1
 
+
+    # Define a filename
     drawing_filename = f"{utils.replace_special_chars(arguments.location)}_BGP_Diagram"
     outformat = "jpg"
     create_bgp_diagram(data, filename=drawing_filename, outformat=outformat)
     print(f"\nDiagram saved as {drawing_filename}.{outformat}")
 
     # Step 3  Render the template
-
     # Render the template with the BGP data
+    # Tip: Look at the template and see what template variables its expecting you to send
+    # Hint:
     rendered_config = bgp_rpt_template.render(
-        location=arguments.location,
-        bgp_list=data,
-        all_peers_up=all_peers_up,
-        ibgp_count=ibgp_count,
-        ebgp_count=ebgp_count,
+
+
+
+
+
         drawing_filename=f"{drawing_filename}.{outformat}",
     )
 
@@ -163,8 +113,9 @@ def main():
     print(rendered_config)
     print("===============================================\n")
 
+    # FINAL STEP: Save the file using a filename like XXXLocation_BGP_REPORT.md
     utils.save_file(
-        f"{utils.replace_special_chars(arguments.location)}_BGP_REPORT.md",
+        f"{ }_BGP_REPORT.md",
         rendered_config,
     )
 
